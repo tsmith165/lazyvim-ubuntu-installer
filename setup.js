4678891d1ac1e63d895e8c7d8ccc14ebae23fa55
tsmith165 <torreysmith165@gmail.com>
2024-03-20 21:10:19 -0700

    updating to use <leader>cf

--- setup.js
+++ setup.js
@@ -104,7 +104,7 @@ vim.keymap.set("n", "+y", '"+y')
 vim.keymap.set("n", "+p", '"+p')
 
 -- Copy file contents and path to system clipboard
-vim.keymap.set("n", "<leader>ai", function()
+vim.keymap.set("n", "<leader>cf", function()
   local file_path = vim.fn.expand("%:p")
   local file_contents = table.concat(vim.fn.readfile(file_path), "\\n")
   local copied_text = "// " .. file_path .. "\\n\\n\\\`\\\`\\\`\\n" .. file_contents .. "\\n\\\`\\\`\\\`"
