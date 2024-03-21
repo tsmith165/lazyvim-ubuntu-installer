7d1b9dd929857984444135ee781495c2ea21d471
tsmith165 <torreysmith165@gmail.com>
2024-03-20 20:14:11 -0700

    removing ocs52

--- utils/pluginConfigs.js
+++ utils/pluginConfigs.js
@@ -30,24 +30,6 @@ const pluginConfigs = [
 },
 `,
     },
-    {
-        name: 'OSC 52 Yank',
-        config: `
-{
-  "osc52.vim",
-  config = function()
-    local function osc52_yank()
-      local buffer = vim.fn.system("base64", vim.fn.getreg("0"))
-      buffer = vim.fn.substitute(buffer, "\\n", "", "")
-      buffer = "\\e]52;c;" .. buffer .. "\\e\\"
-      vim.fn.system("echo -ne " .. vim.fn.shellescape(buffer) .. " > " .. vim.fn.shellescape(vim.g.tty))
-    end
-
-    vim.keymap.set("n", "<Leader>y", osc52_yank, { silent = true })
-  end,
-},
-`,
-    },
 ];
 
 module.exports = pluginConfigs;
