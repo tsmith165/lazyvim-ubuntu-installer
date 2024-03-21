f758cbdec8099527a58d89ec3ca16f7b384e42f7
tsmith165 <torreysmith165@gmail.com>
2024-03-21 06:42:10 -0700

    removing backticks from <leader>cf

--- imports/keymaps.js
+++ imports/keymaps.js
@@ -1,3 +1,7 @@
+const copiedTextString = `
+  local copied_text = "// " .. file_path .. "\\n" .. file_contents .. "\\n\\n"
+`;
+
 const keymaps = `
 -- Copy and paste using xsel
 vim.keymap.set("n", "+y", '"+y')
@@ -7,7 +11,7 @@ vim.keymap.set("n", "+p", '"+p')
 vim.keymap.set("n", "<leader>cf", function()
   local file_path = vim.fn.expand("%:p")
   local file_contents = table.concat(vim.fn.readfile(file_path), "\\n")
-  local copied_text = "// " .. file_path .. "\\n\\n\`\`\`\\n" .. file_contents .. "\\n\`\`\`"
+  ${copiedTextString}
   vim.fn.setreg("+", copied_text)
   vim.notify("Copied file contents and path to system clipboard")
 end)
