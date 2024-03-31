3a8a03b62ce3d6ccde0fbf60f3bc9e85b5501f17
Torrey Smith <tas.smity@yahoo.com>
2024-03-31 14:10:15 -0700

    Adding check to ensure that link does not exist before creating

--- imports/install_steps.js
+++ imports/install_steps.js
@@ -64,10 +64,14 @@ const installSteps = [
         installCommands: [
             'sudo apt-get install -y python2.7 python3.9',
             'mkdir -p /root/tools',
-            'if [ ! -e /root/tools/python27 ]; then sudo ln -s /usr/bin/python2.7 /root/tools/python27; fi',
-            'if [ ! -e /root/tools/python39 ]; then sudo ln -s /usr/bin/python3.9 /root/tools/python39; fi',
-            'if [ ! -e /usr/local/bin/py2 ]; then sudo ln -s /root/tools/python27 /usr/local/bin/py2; fi',
-            'if [ ! -e /usr/local/bin/py3 ]; then sudo ln -s /root/tools/python39 /usr/local/bin/py3; fi',
+            'if [ -e /root/tools/python27 ]; then sudo rm /root/tools/python27; fi',
+            'if [ -e /root/tools/python39 ]; then sudo rm /root/tools/python39; fi',
+            'sudo ln -s /usr/bin/python2.7 /root/tools/python27',
+            'sudo ln -s /usr/bin/python3.9 /root/tools/python39',
+            'if [ -e /usr/local/bin/py2 ]; then sudo rm /usr/local/bin/py2; fi',
+            'if [ -e /usr/local/bin/py3 ]; then sudo rm /usr/local/bin/py3; fi',
+            'sudo ln -s /root/tools/python27 /usr/local/bin/py2',
+            'sudo ln -s /root/tools/python39 /usr/local/bin/py3',
         ],
         postCheck: [
             {
