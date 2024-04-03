09d893d356fa3a981f2ef819bd3ceb4c7fafb0ca
Torrey Smith <tas.smity@yahoo.com>
2024-04-02 17:16:46 -0700

    Updated to use npm to install bun and to install snapd

--- ubuntu_wsl_setup.sh
+++ ubuntu_wsl_setup.sh
@@ -88,7 +88,11 @@ install_bun() {
 install_vscode() {
   if ! command -v code &> /dev/null; then
     log_installing "Visual Studio Code"
-    sudo snap install --classic code
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt update
+    sudo apt install -y code
     log_success "Visual Studio Code installed"
   else
     log_installed "Visual Studio Code"
